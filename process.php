6cc39200d09c45b8e6209b2870fd4e78b1c3f744
Julian Wise <julianadamwise@gmail.com>
2015-05-19 16:51:46 +1000

    Document Touch up

--- process.php
+++ process.php
@@ -3,35 +3,27 @@ include 'database.php';
 
 //Check if form is submitted
 if(isset($_POST['submit'])){
-	$user = mysqli_real_escape_string($con, $_POST['user']);
-	$message = mysqli_real_escape_string($con, $_POST['message']);
+    $user = mysqli_real_escape_string($con, $_POST['user']);
+    $message = mysqli_real_escape_string($con, $_POST['message']);
 
-	//Set timezone
-	date_default_timezone_set('Australia/Sydney');
-	$time = date('h:i:s a',time());
-	
-	if(!isset($user) || $user == '' || !isset($message) || $message =='' ){
-		$error = 'Please fill in your name and a message';
-		header('Location: index.php?error='.urlencode($error));
-		exit();
-	}else{
-		$query = "INSERT INTO shouts (user, message, time)
-				VALUES('$user','$message','$time')";
-				
-		if(!mysqli_query($con,$query)){
-			die('Error : '.mysqli_error($con));
-		}else{
-			header('Location: index.php'); //redirect back to index
-			exit();
-		}
-		
-		}
-	
-	
-	
-	
-	
-	}
+    //Set timezone
+    date_default_timezone_set('Australia/Sydney');
+    $time = date('h:i:s a',time());
 
+    if(!isset($user) || $user == '' || !isset($message) || $message =='' ){
+            $error = 'Please fill in your name and a message';
+            header('Location: index.php?error='.urlencode($error));
+            exit();
+    }else{
+            $query = "INSERT INTO shouts (user, message, time)
+                            VALUES('$user','$message','$time')";
 
+            if(!mysqli_query($con,$query)){
+                    die('Error : '.mysqli_error($con));
+            }else{
+                    header('Location: index.php'); //redirect back to index
+                    exit();
+            }
+        }
+}
 ?>
